cace8148a3814f0fb6253326a653a10a608f6525
petrikpopov <112963523+petrikpopov@users.noreply.github.com>
2023-03-27 21:32:05 +0300

    Add files via upload

---
+++ Human.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using static Decorator_27._03._2023.Program;
+
+namespace Decorator_27._03._2023
+{
+    public class Human : Person
+    {
+        public Human():base("Герой человек")
+        { }
+        public override int Ataca()
+        { return 10; }
+        public override int Speed()
+        { return 10; }
+        public override int Health()
+        { return 10; }
+        public override int Protection()
+        { return 10; }
+    }
+    public class HumanDecorator: Human
+    {
+        Person person;
+
+        public HumanDecorator(Person p)
+        {
+            person = p;
+        }
+        
+    }
+    public class Human_Voin : HumanDecorator
+    {
+        public Human_Voin(Person p) : base(p)
+        {
+
+        }
+        public override int Ataca()
+        { return person.Ataca() + 10 ; }
+        public override int Speed()
+        { return person.Speed() - 10; }
+        public override int Health()
+        { return person.Health() + 50; }
+        public override int Protection()
+        { return person.Protection() - 20; }
+    }
+    public class Human_Mag : HumanDecorator
+    {
+        public Human_Mag(Person p) : base(p)
+        {
+
+        }
+        public override int Ataca()
+        { return person.Ataca() + 20; }
+        public override int Speed()
+        { return person.Speed() - 10; }
+        public override int Health()
+        { return person.Health() + 50; }
+        public override int Protection()
+        { return person.Protection() + 10; }
+    }
+    public class Human_Arbalet : HumanDecorator
+    {
+        public Human_Arbalet(Person p) : base(p)
+        {
+
+        }
+        public override int Ataca()
+        { return person.Ataca() + 20; }
+        public override int Speed()
+        { return person.Speed() + 10; }
+        public override int Health()
+        { return person.Health() + 50; }
+        public override int Protection()
+        { return person.Protection() + 10; }
+    }
+    public class Human_Gold : HumanDecorator
+    {
+        public Human_Gold(Person p) : base(p)
+        {
+
+        }
+        public override int Ataca()
+        { return person.Ataca() + 70; }
+        public override int Speed()
+        { return person.Speed() + 20; }
+        public override int Health()
+        { return person.Health() + 0; }
+        public override int Protection()
+        { return person.Protection() + 0; }
+    }
+    public class Good_human : HumanDecorator
+    {
+        public Good_human(Person p) : base(p)
+        {
+
+        }
+        public override int Ataca()
+        { return person.Ataca() + 50; }
+        public override int Speed()
+        { return person.Speed() + 30; }
+        public override int Health()
+        { return person.Health() + 100; }
+        public override int Protection()
+        { return person.Protection() + 30; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Decorator_27._03._2023
+{
+    public class Program
+    {
+        public abstract class Person
+        {
+            public string Name_Person { set; get; }
+
+            public Person(string NP)
+            {
+                Name_Person = NP;
+            }
+            public abstract int Ataca();
+            public abstract int Speed();
+            public abstract int Health();
+            public abstract int Protection();
+        }
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+
+            Person person = new Elf();
+            Person person1 = new Elf();
+            Person person2 = new Elf();
+            Person person3 = new Elf();
+            Person person4 = new Elf();
+            person = new Elf_Voin(person);
+            person1 = new Elf_Mag(person1);
+            person2 = new Elf_Arbalet(person2);
+            person3 = new Gold_Elf(person3);
+            person4 = new Good_elf(person4);
+
+            Person person0 = new Human();
+            Person person5 = new Human();
+            person5 = new Human_Voin(person5);
+            Person person6 = new Human();
+            person6 = new Human_Mag(person6);
+            Person person7 = new Human();
+            person7 = new Human_Gold(person7);
+            Person person8 = new Human();
+            person8 = new Human_Arbalet(person8);
+            Person person9 = new Human();
+            person9 = new Good_human(person9);
+
+            Console.WriteLine(person.Name_Person);
+            Console.Write("Значения эльфа воина -->");
+            Console.WriteLine(person.Ataca()+"\t"+person.Speed()+"\t"+person.Health()+"\t"+person.Protection());
+            Console.Write("Значения эльфа мага -->");
+            Console.WriteLine(person1.Ataca() + "\t" + person1.Speed() + "\t" + person1.Health() + "\t" + person1.Protection());
+            Console.Write("Значения эльфа арбалетчика -->");
+            Console.WriteLine(person2.Ataca() + "\t" + person2.Speed() + "\t" + person2.Health() + "\t" + person2.Protection());
+            Console.Write("Значения золотого эльфа -->");
+            Console.WriteLine(person3.Ataca() + "\t" + person3.Speed() + "\t" + person3.Health() + "\t" + person3.Protection());
+            Console.Write("Значения доброго эльфа -->");
+            Console.WriteLine(person4.Ataca() + "\t" + person4.Speed() + "\t" + person4.Health() + "\t" + person4.Protection());
+
+            Console.WriteLine(person0.Name_Person);
+            Console.Write("Значения человека воина -->");
+            Console.WriteLine(person5.Ataca() + "\t" + person5.Speed() + "\t" + person5.Health() + "\t" + person5.Protection());
+            Console.Write("Значения человека мага -->");
+            Console.WriteLine(person6.Ataca() + "\t" + person6.Speed() + "\t" + person6.Health() + "\t" + person6.Protection());
+            Console.Write("Значения золотого человека -->");
+            Console.WriteLine(person7.Ataca() + "\t" + person7.Speed() + "\t" + person7.Health() + "\t" + person7.Protection());
+            Console.Write("Значения человека арбалет -->");
+            Console.WriteLine(person8.Ataca() + "\t" + person8.Speed() + "\t" + person8.Health() + "\t" + person8.Protection());
+            Console.Write("Значения хорошего человека -->");
+            Console.WriteLine(person9.Ataca() + "\t" + person9.Speed() + "\t" + person9.Health() + "\t" + person9.Protection());
+
+        }
+    }
+}
